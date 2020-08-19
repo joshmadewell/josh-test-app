fd00df1ad93b786cc4114b50560b42bc9744b4b9
Josh Madewell <josh.madewell@gmail.com>
2020-08-19 15:07:56 -0500

    initial commit

---
+++ functions/function0/source.js
@@ -0,0 +1,3 @@
+exports = function(input){
+  return [{ firstName: "josh",  "last-name": "madewell", "address": BSON.ObjectId("5f0c9269feb6fe1d4dc6f79c") }, { firstName: "josh",  "last-name": "madewell2", "address": BSON.ObjectId("5f0c9269feb6fe1d4dc6f79c") }];
+};
